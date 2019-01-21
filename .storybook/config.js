f7b97f597a026066e7e483152f52fdeb186a219d
Zeb Pykosz <zeb.apy@gmail.com>
2019-01-20 19:35:34 -0500

    add storybook options for url

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,12 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import { withOptions } from '@storybook/addon-options';
+
+addDecorator(
+  withOptions({
+    name: 'react mtg playtester',
+    url: 'https://github.com/fateseal/react-mtg-playtester'
+  })
+);
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.stories.js$/);
